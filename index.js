b9ba21b25385cccc8ef14e57fa7d96ba66dd27b9
TROLLOMEME <upm8877@gmail.com>
2025-05-06 20:07:53 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Welcome to TROLLO Chat</h1>
+      <p><a href="/chat.html">Go to Chat</a></p>
+    </div>
+  );
+}
